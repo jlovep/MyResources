084ed9377e9d81567c5e2389297d7664568ebceb
MyResources <1144220809@qq.com>
2016-05-24 10:27:58 +0800

    提交 代码

--- app/src/main/java/com/apple/myresources/MainActivity.java
+++ app/src/main/java/com/apple/myresources/MainActivity.java
@@ -2,6 +2,7 @@ package com.apple.myresources;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("laopo", "你看看能更新到这个数据吗");
     }
 }
